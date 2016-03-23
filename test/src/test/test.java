f832590a5757f6ea8b01d16e48ab5385001183fd
Yibo Wei <yibo.wei@finpacific.com>
2016-03-23 15:45:11 +1100

    manager change

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -5,11 +5,5 @@ import java.awt.print.Printable;
 public class test {
 	public static void main(){
 		System.out.println("dasdasda");
-<<<<<<< HEAD
-		System.out.println("manager");
-		SYstem.out.println("william123");
-=======
-		System.out.println("anson print");
->>>>>>> anson
 	}
 }
