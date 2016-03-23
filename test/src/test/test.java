1234091d5188d234cdf4d6cc902d36276754ea2f
Yibo Wei <yibo.wei@finpacific.com>
2016-03-23 14:10:38 +1100

    manager add

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -5,14 +5,6 @@ import java.awt.print.Printable;
 public class test {
 	public static void main(){
 		System.out.println("dasdasda");
-<<<<<<< HEAD
-<<<<<<< HEAD
-		SYstem.out.println("william");
-=======
-		System.out.println("anson");
->>>>>>> anson
-=======
-		System.out.println("anson123");
->>>>>>> anson
+		System.out.println("manager");
 	}
 }
